4ed48f1b54cd0bdcc9e1b7a5b6cd89517bd66742
fred <domaine75@gmail.com>
2023-07-18 19:25:00 +0200

    Ajoutez des fichiers projet.

---
+++ Aliveness/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Aliveness
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      Display("testing aliveness of the VM");
+      while (true)
+      {
+        Thread.Sleep(60000);
+        WriteToFile();
+        Display($"{Environment.MachineName} is alive at {DateTime.Now}");
+      }
+
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private static void WriteToFile()
+    {
+      try
+      {
+        using (StreamWriter sw = new StreamWriter("alive.txt", true))
+        {
+          sw.WriteLine($"{Environment.MachineName} is alive at {DateTime.Now}");
+        }
+      }
+      catch (Exception)
+      {
+      }
+    }
+  }
+}
